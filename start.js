15faa76dd4b4497e473a9b76d8da7e0a2e127613
Arthur Yidi <arthuryidi@gmail.com>
2017-01-26 12:15:02 -0800

    disable removing the build when using start

--- start.js
+++ start.js
@@ -4,13 +4,10 @@ var paths = require('./paths.js');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config.js');
-var fs = require('fs-extra');
 
 config.entry.app.unshift('webpack-dev-server/client?http://localhost:8080/');
 
 function run() {
-  fs.emptyDirSync(paths.build);
-
   var compiler = webpack(config);
   var devServer = new WebpackDevServer(compiler, {
     compress: true,
